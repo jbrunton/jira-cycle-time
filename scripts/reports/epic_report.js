dcb9503e7e7611799911364b0c280c3f1bd99dc2
John Brunton <john_brunton@hotmail.co.uk>
2014-11-08 16:04:14 +0000

    include links to issues in report

--- scripts/reports/epic_report.js
+++ scripts/reports/epic_report.js
@@ -3,6 +3,7 @@ var _ = require('lodash');
 
 var Class = require('../shared/class');
 var BaseReport = require('./base_report');
+var epicReportTemplate = require('./templates/epic_report.hbs');
 
 module.exports = EpicReport;
 
@@ -18,12 +19,10 @@ function EpicReport(jiraClient) {
 EpicReport.CHART_CONTENT = 'Hello';
 
 EpicReport.prototype.render = function(target) {
-  var appendIssue = function(issue) {
-    $(target).append('<p>' + issue.key + '</p>');
-  };
-  
   return this.jiraClient.search().then(function(issues) {
-    _(issues).each(appendIssue);
+    $(target).append(epicReportTemplate({
+      issues: issues
+    }));
   });
 }
 
