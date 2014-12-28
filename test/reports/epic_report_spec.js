0f5c691401c61897f5710b818b247ed137909a6c
John Brunton <john_brunton@hotmail.co.uk>
2014-12-28 22:02:12 +0000

    #19 fix spec for EpicReport#render

--- test/reports/epic_report_spec.js
+++ test/reports/epic_report_spec.js
@@ -1,6 +1,9 @@
+var moment = require('moment');
+
 var EpicReport = require('../../scripts/reports/epic_report');
 var JiraClient = require('../../scripts/jira/jira_client'); // TODO: factory this
 var Validator = require('../../scripts/shared/validator');
+require('../../scripts/ui/helpers/epic_list_helper');
 
 describe ('EpicReport', function() {
   
@@ -23,9 +26,11 @@ describe ('EpicReport', function() {
   });
   
   describe ('render', function() {
-    xit ("renders a list of issues", function(done) {
-      var expectedEpics = [{ key: 'DEMO-101' }];
-      spyOn(jiraClient, 'getEpics').and.returnValue(Q(expectedEpics));
+    it ("renders a list of issues", function(done) {
+      var expectedEpics = [{
+        key: 'DEMO-101 [S]', completedDate: moment()
+      }];
+      spyOn(report, 'loadEpics').and.returnValue(Q(expectedEpics));
 
       report.render(dom).then(function() {      
         expect(dom).toContainText('DEMO-101');
